874c5d465219b18981290a0c56acbb7f82185ede
Tanaka Nyabanga <tanakawork801@gmail.com>
2024-11-18 18:55:52 +0200

    Update AppNavigator.js

--- src/Navigation/AppNavigator.js
+++ src/Navigation/AppNavigator.js
@@ -1,76 +1,100 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Image, View, Text, StyleSheet } from 'react-native';
 import Home from '../Screen/Home';
 import MenuStack from '../Screen/MenuStack';
 import AccountStack from '../Account/AccountStack';
 import PodStack from '../Screen/PodsStack';
-import Pods from '../Screen/Pods';
-import PodDetail from '../Screen/PodDetail';
-import BookingProcess from '../Screen/BookingProcess';
-import AccountInfo from '../Account/AccountInfo';
-import Cart from '../Screen/Cart';
-import { Image } from 'react-native';
+import CartStack from '../Screen/CartStack';
+import { useCart } from '../Screen/CartContext';
 
-// Create Tab and Stack Navigators
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-// Stack Navigator for the Pods tab to include PodDetail
-const PodsStackNavigator = () => {
-  return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Pods" component={Pods} />
-      <Stack.Screen name="PodDetail" component={PodDetail} />
-      <Stack.Screen name="BookingProcess" component={BookingProcess} />
-      <Stack.Screen name="AccountInfo" component={AccountInfo} />
-      <Stack.Screen name="Account" component={Account} />
-      <Stack.Screen name="Cart" component={Cart} />
-    </Stack.Navigator>
-  );
-};
-
 const AppNavigator = () => {
+  const { cartCount } = useCart(); // Access cart count from context
+
   return (
     <Tab.Navigator>
-      <Tab.Screen 
-        name="Home" 
-        component={Home} 
+      <Tab.Screen
+        name="Home"
+        component={Home}
         options={{
           tabBarIcon: () => (
             <Image source={require('../assets/navHome.png')} style={{ width: 30, height: 30 }} />
           ),
-        }} 
+        }}
       />
-      <Tab.Screen 
-        name="Menu" 
-        component={MenuStack} 
+      <Tab.Screen
+        name="Menu"
+        component={MenuStack}
         options={{
           tabBarIcon: () => (
             <Image source={require('../assets/navMenu.png')} style={{ width: 30, height: 30 }} />
           ),
-        }} 
+        }}
       />
-      <Tab.Screen 
-        name="Pods" 
-        component={PodStack} 
+      <Tab.Screen
+        name="Pods"
+        component={PodStack}
         options={{
           tabBarIcon: () => (
             <Image source={require('../assets/navSleepPods.png')} style={{ width: 30, height: 30 }} />
           ),
-        }} 
+        }}
       />
-      <Tab.Screen 
-        name="Account" 
-        component={AccountStack} 
+      <Tab.Screen
+        name="Cart"
+        component={CartStack}
+        options={{
+          tabBarIcon: () => (
+            <View style={styles.iconWrapper}>
+              <Image source={require('../assets/basket.png')} style={{ width: 30, height: 30 }} />
+              {cartCount > 0 && (
+                <View style={styles.cartBadge}>
+                  <Text style={styles.cartBadgeText}>{cartCount}</Text>
+                </View>
+              )}
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={AccountStack}
         options={{
           tabBarIcon: () => (
             <Image source={require('../assets/navUser.png')} style={{ width: 30, height: 30 }} />
           ),
-        }} 
+        }}
       />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  iconWrapper: {
+    position: 'relative',
+    width: 30,
+    height: 30,
+  },
+  cartBadge: {
+    position: 'absolute',
+    top: -5,
+    right: -5,
+    backgroundColor: 'red',
+    borderRadius: 10,
+    padding: 2,
+    minWidth: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cartBadgeText: {
+    color: 'white',
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+});
 
 export default AppNavigator;
